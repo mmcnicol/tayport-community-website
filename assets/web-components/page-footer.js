a27e114601a0ca4d1d8cadad2fcc2f56c892077d
Matthew McNicol <matthew.mcnicol@gmail.com>
2018-03-31 17:36:20 +0100

    fixed google analytics script not being executed - page footer

--- assets/web-components/page-footer.js
+++ assets/web-components/page-footer.js
@@ -14,9 +14,17 @@ class PageFooter extends HTMLElement {
 
     var footerHtml = `<p>Tayport Community Website<br /><br />Supported by Tayport Community Council [April 2003 – present]<br /></p>`;
 
-    var footerScript = `
+    //this.shadowRoot.innerHTML = footerHtml;
+    this.innerHTML = footerHtml;
+
+  }
+
+}
+
+customElements.define('page-footer', PageFooter);
+
+
 <!-- google analytics -->
-<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -24,16 +32,4 @@ class PageFooter extends HTMLElement {
 
   ga('create', 'UA-274412-4', 'auto');
   ga('send', 'pageview');
-
-</script>
-`;
-
-    //this.shadowRoot.innerHTML = footerHtml + footerScript;
-    this.innerHTML = footerHtml + footerScript;
-
-  }
-
-}
-
-customElements.define('page-footer', PageFooter);
 
